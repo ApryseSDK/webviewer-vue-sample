36e76890b8f9daf9434a9b938c4f8f05934d0b7d
Hussam Barouqa <hussam.barouqa@apryse.com>
2024-09-24 10:14:51 +0300

    Fixed ESlint Config
    - Removed spaces from files key which was preventing linting from running properly on VUE files.
    - Applied linting rules on VUE files.

--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -3,11 +3,11 @@
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-import WebViewer from "@pdftron/webviewer";
+import { ref, onMounted } from 'vue';
+import WebViewer from '@pdftron/webviewer';
 
 export default {
-  name: "WebViewer",
+  name: 'WebViewer',
   props: { initialDoc: { type: String } },
   setup(props) {
     const viewer = ref(null);
@@ -22,7 +22,7 @@ export default {
           const { documentViewer, annotationManager, Annotations } =
             instance.Core;
 
-          documentViewer.addEventListener("documentLoaded", () => {
+          documentViewer.addEventListener('documentLoaded', () => {
             const rectangleAnnot = new Annotations.RectangleAnnotation({
               PageNumber: 1,
               // values are in page coordinates with (0, 0) in the top left
